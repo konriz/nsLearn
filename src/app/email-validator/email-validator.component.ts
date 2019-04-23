81bf0c9392ea4e48e54a530923b9312fad6a489d
Unknown <konriz@gmail.com>
2019-04-23 10:03:00 +0200

    InitFiles

---
+++ src/app/email-validator/email-validator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-email-validator",
+    moduleId: module.id,
+    templateUrl: "./email-validator.component.html"
+})
+export class EmailValidatorComponent {
+    constructor() { }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-items",
+    moduleId: module.id,
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent {
+    constructor() { }
+}
